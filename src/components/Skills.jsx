ae391ac5f1c7a66199dd7e2f6f0db8c10b315ea0
KevinJurado <kjuradoe@gmail.com>
2024-01-25 15:46:16 -0500

    skills changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,15 +5,46 @@ const Skills = () => {
     <div className="skills" id="skills">
       <h2 className="skills__title">Habilidades</h2>
       <div className="skills__icons">
-        <img src="/html.svg" alt="" />
-        <img src="/css.svg" alt="" />
-        <img src="/javascript.svg" alt="" />
-        <img src="/typescript.svg" alt="" />
-        <img src="/react.svg" alt="" />
-        <img src="/java.svg" alt="" />
-        <img src="/git.svg" alt="" />
-        <img src="/github.svg" alt="" />
-        <img src="/figma.svg" alt="" />
+        <div>
+          <img src="/html.svg" alt="" />
+          <p>HTML</p>
+        </div>
+        <div>
+          <img src="/css.svg" alt="" />
+          <p>CSS</p>
+        </div>
+        <div>
+          <img src="/javascript.svg" alt="" />
+          <p>JavaScript</p>
+        </div>
+        <div>
+          <img src="/typescript.svg" alt="" />
+          <p>TypeScript</p>
+        </div>
+        <div>
+          <img src="/react.svg" alt="" />
+          <p>React.JS</p>
+        </div>
+        <div>
+          <img src="/angular.svg" alt="" />
+          <p>Angular</p>
+        </div>
+        <div>
+          <img src="/java.svg" alt="" />
+          <p>Java</p>
+        </div>
+        <div>
+          <img src="/git.svg" alt="" />
+          <p>Git</p>
+        </div>
+        <div>
+          <img src="/github.svg" alt="" />
+          <p>GitHub</p>
+        </div>
+        <div>
+          <img src="/node.svg" alt="" />
+          <p>Node.JS</p>
+        </div>
       </div>
     </div>
   )
